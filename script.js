9af2637ee3c2c275f4e426ebee0afe03980dfb07
surajmishra45 <158718420+surajmishra45@users.noreply.github.com>
2025-10-02 21:28:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,10 +14,42 @@ function convertToRoman(num) {
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
+function convertToRoman(num) {
+  if (num === 0) return "N"; // Romans used 'N' (nulla) for zero
 
+  // Roman numeral mapping
+  const romanMap = [
+    ["M", 1000],
+    ["CM", 900],
+    ["D", 500],
+    ["CD", 400],
+    ["C", 100],
+    ["XC", 90],
+    ["L", 50],
+    ["XL", 40],
+    ["X", 10],
+    ["IX", 9],
+    ["V", 5],
+    ["IV", 4],
+    ["I", 1],
+  ];
 
+  let result = "";
 
+  for (let [roman, value] of romanMap) {
+    while (num >= value) {
+      result += roman;
+      num -= value;
+    }
+  }
+
+  return result;
+}
+
+// Example tests
+console.log(convertToRoman(14));   // XIV
+console.log(convertToRoman(798));  // DCCXCVIII
+console.log(convertToRoman(10000)); // MMMMMMMMMM
 
 // do not edit below this line
-module.exports = convertToRoman
+module.exports = convertToRoman;
